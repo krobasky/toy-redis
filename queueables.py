47e98b932d86807f194debb82b5cb169e19bf22a
Kimberly Robasky <krobasky@gmail.com>
2022-01-08 18:16:18 -0500

    removed extraneous comments

--- queueables.py
+++ queueables.py
@@ -1,7 +1,3 @@
 def stdout_queue_item(outitem):
     print( "[ {0} ] *** executed stdout_queueble_item ***".format(str(outitem)) )
     return 100;
-
-#def file_queueble_item(i:int):
-#    with open("redis_data/f.txt","w+") as f:
-#        f.write("executed f")
